8bef2d4c331a1308ad1330e5c17a901c87f42a1b
akashp69 <akashpakhare499@gmail.com>
2022-08-21 18:42:24 +0530

    Added: Refactor to create generic class

--- src/com/generics/MaximumNumber.java
+++ src/com/generics/MaximumNumber.java
@@ -1,17 +1,27 @@
 package com.generics;
 
-public class MaximumNumber {
+public class MaximumNumber <T extends Comparable>{
+    T var1, var2, var3;
+    public MaximumNumber(T var1, T var2, T var3) {
+        this.var1 = var1;
+        this.var2 = var2;
+        this.var3 = var3;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Generic Program.");
-        Integer a = 25, b = 50, c = 100;
+        Integer a = 25, b = 50, c =100;
         Float x = 15.2f, y = 15.8f, z = 15.6f;
         String j = "Apple", k = "Peach" , l = "Banana";
 
-        MaximumNumber.getMaximum(a, b, c);
-        MaximumNumber.getMaximum(x, y, z);
-        MaximumNumber.getMaximum(j, k, l);
+        new MaximumNumber<>(a, b, c).max();
+        new MaximumNumber<>(x, y, z).max();
+        new MaximumNumber<>(j, k, l).max();
     }
 
+    private void max() {
+
+        MaximumNumber.getMaximum(var1, var2, var3);
+    }
     private static <T extends Comparable> void getMaximum(T a, T b, T c) {
         T max = a;
         if (b.compareTo(a) > 0) {
